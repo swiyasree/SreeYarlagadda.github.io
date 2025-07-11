0523e1b5d8911da3459a0a3d2e5c13e124a14bcb
Sree <ssyarlag@usc.edu>
2025-07-11 13:50:55 -0700

    Personal Portfolio

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    new Typed("#typed", {
+      strings: ["Dreamer.", "Doer.", "Believer."],
+      typeSpeed: 60,
+      backSpeed: 40,
+      backDelay: 1500,
+      startDelay: 500,
+      loop: true
+    });
+  });
+  
